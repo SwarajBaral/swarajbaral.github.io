0841de9dad4ba08b1bf5e8cf636785593a249af4
SwarajBaral <baralswaraj40@gmail.com>
2022-05-21 13:22:22 +0530

    Added icons in homepage

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,12 @@ window.onscroll = () => {
       resumeButton.classList.remove('cta-scroll')
     }
 }
+
+var options = {
+   strings: ['Web apps','Scalable backends','Fullstack Applications'],
+   typeSpeed: 60,
+   backSpeed: 20,
+   smartBackspace: true
+ };
+ 
+ var typed = new Typed('.typed-element', options);
